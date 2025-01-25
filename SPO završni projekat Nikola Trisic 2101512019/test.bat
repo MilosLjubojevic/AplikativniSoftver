30ad0d7248bd1eadf5d269a1af0cd7ac5addefc5
MilosLjubojevic <142318299+MilosLjubojevic@users.noreply.github.com>
2025-01-25 14:13:50 +0100

    Add files via upload

---
+++ SPO završni projekat Nikola Trisic 2101512019/test.bat
@@ -0,0 +1,3 @@
+set CLASSPATH=.; .\lib\plugin.jar
+java.exe -jar e2gRuleEngine.jar -w=400 -h=400 -bg=#FFFF00 -tc="#3399FF" -at "Preporuka kupona u vozilu" -st "Preporuka kupona u vozilu" -sb "Pokreni" -kb file:preporuka_kupona_u_vozilu.kb
+
