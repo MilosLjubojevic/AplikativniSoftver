e1ce4c874de298682c987238e24bff32ed5ec866
MilosLjubojevic <142318299+MilosLjubojevic@users.noreply.github.com>
2025-01-25 14:17:30 +0100

    Add files via upload

---
+++ aplikativni softver TRISIC/admin/templates/footer.php
@@ -0,0 +1,5 @@
+<body>
+    <footer class="section">
+        <div class="center grey-text"> Nikola Trišic I sva prava rezervisana</div>
+    </footer>
+</body>
---
+++ aplikativni softver TRISIC/ponude.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>ponude</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<<section class="header">
+
+<a href="pocetna.php" class="logo">putuj.</a>
+
+<nav class="navbar">
+   <a href="pocetna.php">pocetna</a>
+   <a href="onama.php">o nama</a>
+   <a href="ponude.php">ponude</a>
+   <a href="rezervisi.php">rezervisi</a>
+</nav>
+
+<div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-2.png) no-repeat">
+   <h1>ponude</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">najbolje destinacije</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>avantura & obilazak</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="rezervisi.php" class="btn">rezervisi sada</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>avantura & obilazak</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="rezervisi.php" class="btn">rezervisi sada</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>avantura & obilazak</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="rezervisi.php" class="btn">rezervisi sada</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-4.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>avantura & obilazak</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="rezervisi.php" class="btn">rezervisi sada</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-5.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>avantura & obilazak</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="rezervisi.php" class="btn">rezervisi sada</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-6.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>avantura & obilazak</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="rezervisi.php" class="btn">rezervisi sada</a>
+         </div>
+      </div>
+
+     
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-7.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>avantura & obilazak</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="rezervisi.php" class="btn">rezervisi sada</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-8.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>avantura & obilazak</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="rezervisi.php" class="btn">rezervisi sada</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-9.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>avantura & obilazak</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="rezervisi.php" class="btn">rezervisi sada</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-10.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>avantura & obilazak</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="rezervisi.php" class="btn">rezervisi sada</a>
+         </div>
+      </div>
+
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-11.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>avantura & obilazak</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="rezervisi.php" class="btn">rezervisi sada</a>
+         </div>
+      </div>
+
+
+      
+      
+
+   </div>
+
+   <div class="load-more"><span class="btn">ucitaj vise</span></div>
+
+</section>
+
+<!-- packages section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>brzi linkovi</h3>
+         <a href="pocetna.php"> <i class="fas fa-angle-right"></i> homepocetna</a>
+         <a href="onama.php"> <i class="fas fa-angle-right"></i> o nama</a>
+         <a href="ponude.php"> <i class="fas fa-angle-right"></i> ponude</a>
+         <a href="rezervisi.php"> <i class="fas fa-angle-right"></i> rezervisi</a>
+      </div>
+
+      <div class="box">
+         <h3>dodatni linkovi</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> postavite pitanje</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> o nama</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> polisa privatnosti</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> pravila koristenja</a>
+      </div>
+
+      <div class="box">
+         <h3>kontakt</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +387-66-111-222 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +387-55-222-333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i>trisa.zvezdas@gmail.com</a>
+         <a href="#"> <i class="fas fa-map"></i> bijeljina  </a>
+      </div>
+
+      <div class="box">
+         <h3>zapratite nas</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> whatsapp </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> napravio <span>Nikola Trisic</span> | sva prava rezervisana! </div>
+
+</section>
+
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
